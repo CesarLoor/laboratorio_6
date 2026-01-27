e103ca83f4707071fcabdeca0dc0bd7fe928d2dc
CesarLoor <ciloor2@espe.edu.ec>
2026-01-27 10:35:22 -0500

    prueba de eslint 2

--- resta.js
+++ resta.js
@@ -1,5 +1,32 @@
 function resta(a, b) {
     return a - b
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
 }
 
 // Esta es una línea muy larga que definitivamente va a exceder el límite de 120 caracteres configurado en ESLint para probar la regla max-len
