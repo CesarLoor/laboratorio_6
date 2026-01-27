f139a289fc664ebbe7cb3fa3b62b46942b48752a
CesarLoor <ciloor2@espe.edu.ec>
2026-01-27 09:54:37 -0500

    firs commit

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+export default [
+    {
+        files: ['**/*.js'],
+        ignores: ['node_modules/**'], //esto es para que no me marque errores en node_modules 
+        languageOptions: {
+            ecmaVersion: 'latest',
+            sourceType: 'module'
+        },
+        rules: {
+            semi: ['error', 'always'],
+            quote: ['error', 'single']
+        }
+    }
+]
+
---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get('/',(req, res)=>{
+    res.send("Integración continua");
+});
+
+app.listen(port, () => {
+    console.log(`Servidor trabajando en el puerto ${port}`);
+});
+
