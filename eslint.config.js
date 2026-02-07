394c957dd4fe57c246f8be592609864e161b8180
CesarLoor <ciloor2@espe.edu.ec>
2026-02-06 22:22:08 -0500

    workflow firebase 2

--- eslint.config.js
+++ eslint.config.js
@@ -11,5 +11,5 @@ export default [
             quotes: ['error', 'single'],
         }
     }
-]
+];
 
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express = require('expres');
+const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/',(req, res)=>{
+app.get('/', (req, res) => {
     res.send('Integración continua');
 });
 
